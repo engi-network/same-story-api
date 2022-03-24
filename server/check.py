bf828ba36ba7b97a9d94dc284480c20ceb2f1e4a
CHRISTOPHER KELLY <chris@nourishbalancethrive.com>
2022-03-24 13:30:03 -0700

    now with error.json

--- server/check.py
+++ server/check.py
@@ -1,10 +1,9 @@
 import asyncio
+import gettext
 import json
 import os
-import shutil
 import socket
 import sys
-import tempfile
 from asyncio.subprocess import PIPE
 from contextlib import contextmanager
 from pathlib import Path
@@ -13,6 +12,8 @@ from time import perf_counter
 from helpful_scripts import setup_logging
 
 log = setup_logging()
+
+_ = gettext.gettext
 
 
 @contextmanager
@@ -46,10 +47,10 @@ async def run(cmd):
     return proc.returncode, stdout, stderr
 
 
-async def run_raise(cmd, returncode=0):
+async def run_raise(cmd, returncode=0, msg=None):
     retval = await run(cmd)
     if retval[0] != returncode:
-        raise RuntimeError
+        raise RuntimeError(msg)
     return retval
 
 
@@ -63,6 +64,18 @@ def get_dims(spec):
     height = int(spec.get("height", "600")) // 2
     width = int(spec.get("width", "800")) // 2
     return f"{width}x{height}"
+
+
+e_msg = {
+    "clone": _("failed to clone GitHub repo"),
+    "frame": _("Figma frame missing"),
+    "branch": _("failed to sync GitHub repo, check branch"),
+    "commit": _("failed to checkout commit in GitHub repo"),
+    "install": _("npm install failed"),
+    "storycap": _("storycap failed"),
+    "aws": _("internal AWS error"),
+    "comp": _("failed to generate visual comparison"),
+}
 
 
 async def check(check_id):
@@ -79,64 +92,83 @@ async def check(check_id):
     branch = spec.get("branch")
     commit = spec.get("commit")
     sync = True
-    if not check_code.exists():
-        sync = False
-        await run_raise(f"gh repo clone {check_repo} {check_code}")
-    with set_directory(check_code):
-        if sync:
-            # stash any local changes, e.g. package-lock.json
-            await run_raise(f"git stash")
-            # perform incremental update
-            branch_cmd = f" --branch {branch}" if branch is not None else ""
-            await run_raise(f"gh repo sync{branch_cmd}")
-        if commit:
-            await run_raise(f"git checkout {commit}")
-        await run_raise("npm install")
-        log.info("capturing screenshots")
-        # TODO even when specifying a different port for each run, storycap doesn't work concurrently
-        port = get_port()
-        await run_raise(
-            f"npx storycap http://localhost:{port} --viewport {get_dims(spec)} --serverCmd 'start-storybook -p {port}'"
-        )
-        log.info("uploading code screenshots to s3")
-        await run_raise(
-            f"aws s3 cp {check_code}/__screenshots__ s3://{check_prefix}/report/__screenshots__ --recursive"
-        )
-        log.info("running visual comparisons")
-        check_story = spec["story"]
-        check_component = spec["component"]
-        check_frame = check_dir / f"frames/{check_component}-{check_story}.png"
-        check_code_screenshot = (
-            check_code / f"__screenshots__/Example/{check_component}/{check_story}.png"
-        )
-        assert check_frame.exists()
-        assert check_code_screenshot.exists()
-        blue_difference = "blue_difference.png"
-        log.info("running regression with blue hightlight and uploading")
-        # compare exits with code 1 even though it seems to have run successfully
-        await run(
-            f"compare {check_code_screenshot} {check_frame} -highlight-color blue {blue_difference}"
-        )
-        await run_raise(
-            f"aws s3 cp {blue_difference} s3://{check_prefix}/report/{blue_difference}"
-        )
-        log.info("running regression with gray hightlight and uploading")
+    error = "error.json"
+    try:
+        if not check_code.exists():
+            sync = False
+            await run_raise(f"gh repo clone {check_repo} {check_code}", msg=e_msg["clone"])
+        with set_directory(check_code):
+            if sync:
+                # stash any local changes, e.g. package-lock.json
+                await run_raise(f"git stash", msg=e_msg["clone"])
+                # perform incremental update
+                branch_cmd = f" --branch {branch}" if branch is not None else ""
+                await run_raise(f"gh repo sync{branch_cmd}", msg=e_msg["branch"])
+            if commit:
+                await run_raise(f"git checkout {commit}", msg=e_msg["commit"])
+            await run_raise("npm install", msg=e_msg["install"])
+            log.info("capturing screenshots")
+            # TODO even when specifying a different port for each run, storycap doesn't work concurrently
+            port = get_port()
+            await run_raise(
+                f"npx storycap http://localhost:{port} --viewport {get_dims(spec)} --serverCmd 'start-storybook -p {port}'",
+                msg=e_msg["storycap"],
+            )
+            log.info("uploading code screenshots to s3")
+            await run_raise(
+                f"aws s3 cp {check_code}/__screenshots__ s3://{check_prefix}/report/__screenshots__ --recursive",
+                msg=e_msg["aws"],
+            )
+            log.info("running visual comparisons")
+            check_story = spec["story"]
+            check_component = spec["component"]
+            check_frame = check_dir / f"frames/{check_component}-{check_story}.png"
+            check_code_screenshot = (
+                check_code / f"__screenshots__/Example/{check_component}/{check_story}.png"
+            )
+            assert check_frame.exists(), e_msg["frame"]
+            assert check_code_screenshot.exists(), e_msg["storycap"]
+            blue_difference = Path("blue_difference.png")
+            log.info("running regression with blue hightlight and uploading")
+            # compare exits with code 1 even though it seems to have run successfully
+            await run(
+                f"compare {check_code_screenshot} {check_frame} -highlight-color blue {blue_difference}"
+            )
+            assert blue_difference.exists(), e_msg["comp"]
+            await run_raise(
+                f"aws s3 cp {blue_difference} s3://{check_prefix}/report/{blue_difference}",
+                msg=e_msg["aws"],
+            )
+            log.info("running regression with gray hightlight and uploading")
 
-        gray_difference = "gray_difference.png"
-        await run_raise(
-            f"convert {check_code_screenshot} -flatten -grayscale Rec709Luminance "
-            f"{check_frame} -flatten -grayscale Rec709Luminance "
-            "-clone 0-1 -compose darken -composite "
-            f"-channel RGB -combine {gray_difference}"
-        )
-        await run_raise(
-            f"aws s3 cp {gray_difference} s3://{check_prefix}/report/{gray_difference}"
-        )
-        # compare exits with code 1 even though it seems to have run successfully
-        _, _, stderr = await run(f"compare -metric MAE {check_code_screenshot} {check_frame} null")
-        results = "results.json"
-        json.dump({"MAE": stderr.decode()}, open(results, "w"))
-        await run_raise(f"aws s3 cp {results} s3://{check_prefix}/report/{results}")
+            gray_difference = Path("gray_difference.png")
+            await run_raise(
+                f"convert {check_code_screenshot} -flatten -grayscale Rec709Luminance "
+                f"{check_frame} -flatten -grayscale Rec709Luminance "
+                "-clone 0-1 -compose darken -composite "
+                f"-channel RGB -combine {gray_difference}",
+                msg=e_msg["comp"],
+            )
+            assert gray_difference.exists(), e_msg["comp"]
+            await run_raise(
+                f"aws s3 cp {gray_difference} s3://{check_prefix}/report/{gray_difference}",
+                msg=e_msg["aws"],
+            )
+            # compare exits with code 1 even though it seems to have run successfully
+            _, _, stderr = await run(
+                f"compare -metric MAE {check_code_screenshot} {check_frame} null"
+            )
+            results = "results.json"
+            json.dump({"MAE": stderr.decode()}, open(results, "w"))
+            await run_raise(
+                f"aws s3 cp {results} s3://{check_prefix}/report/{results}", msg=e_msg["aws"]
+            )
+            await run(f"aws s3 rm s3://{check_prefix}/report/{error}")
+    except (AssertionError, RuntimeError) as e:
+        log.exception(e)
+        json.dump({"error": f"{e.__class__.__name__}: {e}"}, open(error, "w"))
+        await run_raise(f"aws s3 cp {error} s3://{check_prefix}/report/{error}")
+
     t1_stop = perf_counter()
     log.info(f"check done {t1_stop - t1_start} seconds")
     return 0
