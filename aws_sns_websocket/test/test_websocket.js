e05b84eaf4d7dc53858a7340151f52ec8dc441d4
CHRISTOPHER KELLY <christopherkelly@engi.network>
2022-05-21 11:11:23 -0700

    read params from env

--- aws_sns_websocket/test/test_websocket.js
+++ aws_sns_websocket/test/test_websocket.js
@@ -1,12 +1,15 @@
 import WebSocket from "ws";
 
-const ws = new WebSocket("wss://aawv1ibk24.execute-api.us-west-2.amazonaws.com/prod");
+const WS_URL = process.env["WS_URL"];
+const CHECK_ID = process.env["CHECK_ID"];
+
+const ws = new WebSocket(WS_URL);
 
 ws.on("open", function open() {
     console.log("connected");
     ws.send(JSON.stringify({
         message: "subscribe",
-        check_id: "2e2883d3-8d9a-445a-80bc-96a6a99cb3e7"
+        check_id: CHECK_ID
     }));
     // ws.close();
 });
