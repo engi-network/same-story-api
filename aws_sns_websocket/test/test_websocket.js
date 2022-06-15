868b15b29cdda922d2b2a5e0c5a6ff49b2694f00
CHRISTOPHER KELLY <christopherkelly@engi.network>
2022-06-15 08:16:37 -0700

    auto reconnect

--- aws_sns_websocket/test/test_websocket.js
+++ aws_sns_websocket/test/test_websocket.js
@@ -5,23 +5,35 @@ import { argv } from 'node:process';
 const WS_URL = process.env["WS_URL"];
 const CHECK_ID = argv[2];
 
-const ws = new WebSocket(WS_URL);
+var retries = 5;
 
-ws.on("open", function open() {
-    console.log(`connected to ${WS_URL}`);
-    console.log(`subscribing to ${CHECK_ID}`);
-    ws.send(JSON.stringify({
-        message: "subscribe",
-        check_id: CHECK_ID
-    }));
-    // ws.close();
-});
+function connect() {
+    var ws;
+    if (--retries == 0) {
+        console.log(`giving up on ${WS_URL}`);
+        process.exit(1);
+    }
+    console.log(`connecting to ${WS_URL}`);
+    ws = new WebSocket(WS_URL);
 
-ws.on("close", function close() {
-    console.log("disconnected");
-});
+    ws.on("open", function open() {
+        console.log(`subscribing to ${CHECK_ID}`);
+        ws.send(JSON.stringify({
+            message: "subscribe",
+            check_id: CHECK_ID
+        }));
+        // ws.close();
+    });
 
-ws.on("message", function message(data) {
-    let message = JSON.parse(data);
-    console.log("received: %s", message);
-});
+    ws.on("close", function close() {
+        console.log("disconnected");
+        connect();
+    });
+
+    ws.on("message", function message(data) {
+        let message = JSON.parse(data);
+        console.log("received: %s", message);
+    });
+}
+
+connect();
