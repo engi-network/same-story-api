5060c72c24decfc224930f6e36c84c920a9e3201
CHRISTOPHER KELLY <christopherkelly@engi.network>
2022-06-21 06:27:23 -0700

    tidy up toy sqs_status_test.js

--- aws_js_test/v3/sqs_status_test.js
+++ aws_js_test/v3/sqs_status_test.js
@@ -1,41 +1,49 @@
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } from "@aws-sdk/client-sqs";
+import pino from "pino";
+
+const logger = pino();
 
 // Set the AWS Region
 const REGION = "us-west-2";
+const QUEUE_URL = process.env["QUEUE_URL"];
+const IDENTITY_POOL_ID = process.env["IDENTITY_POOL_ID"];
 
 const sqsClient = new SQSClient({
     region: REGION,
     credentials: fromCognitoIdentityPool({
         client: new CognitoIdentityClient({ region: REGION }),
-        identityPoolId: process.env["IDENTITY_POOL_ID"]
+        identityPoolId: IDENTITY_POOL_ID
     }),
 });
 
-const queueUrl = process.env["QUEUE_URL"];
+
+const stringify = (thing) => {
+    return JSON.stringify(thing, null, 2);
+}
 
 const processMsg = async (msg) => {
     const msg_ = JSON.parse(msg.Body);
-    console.log("message", msg_.Message);
+    logger.info(`message: ${msg_.Message}`);
     if (msg_.Message.check_id === "") {
         // TODO check the check_id before deleting the status message
     }
     var deleteParams = {
-        QueueUrl: queueUrl,
+        QueueUrl: QUEUE_URL,
         ReceiptHandle: msg.ReceiptHandle
     };
     const data_ = await sqsClient.send(new DeleteMessageCommand(deleteParams));
-    console.log("message deleted", data_);
+    logger.info(`message deleted: ${stringify(data_)}`);
 }
 
 const receiveMessages = async () => {
     const params = {
-        QueueUrl: queueUrl,
+        QueueUrl: QUEUE_URL,
         WaitTimeSeconds: 1,
         MaxNumberOfMessages: 10,
     };
-    console.log("checking for messages");
+    logger.info("checking for messages");
     const data = await sqsClient.send(new ReceiveMessageCommand(params));
     if (data.Messages) {
         await Promise.all(data.Messages.map(processMsg));
@@ -43,8 +51,9 @@ const receiveMessages = async () => {
 }
 
 const run = async () => {
-    console.log("IDENTITY_POOL_ID =", process.env["IDENTITY_POOL_ID"]);
-    console.log("QUEUE_URL =", queueUrl);
+    logger.info(`IDENTITY_POOL_ID='${IDENTITY_POOL_ID}'`);
+    logger.info(`QUEUE_URL='${QUEUE_URL}'`);
     setInterval(receiveMessages, 10000);
 };
+
 run();
